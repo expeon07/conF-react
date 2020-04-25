f7338717d3f61459e62ea6451e0736f290aea824
Sheena Fernandez <sheena.fernandez78@gmail.com>
2020-04-25 21:35:45 +0900

    redux actions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import Contact from './ContactComponent';
 import Dishdetail from './DishdetailComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
 
 
 // maps redux store state into props that will be available to main component
@@ -19,6 +20,10 @@ const mapStateToProps = state => {
       leaders: state.leaders
     }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+});
 
 class Main extends Component {
 
@@ -42,6 +47,7 @@ class Main extends Component {
       return(
         <Dishdetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
           comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}  
+          addComment={this.props.addComment}
         />
       );
     }
@@ -64,4 +70,4 @@ class Main extends Component {
 }
 
 // to connect component to react router
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
